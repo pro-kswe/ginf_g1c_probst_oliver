0ca1656a1816f8bc97f087b0f15fae87517ae6fb
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2025-12-12 08:07:32 +0100

    Emotionen

---
+++ 02_konsole/01_bildschirmausgabe/99_emotionen.py
@@ -0,0 +1,8 @@
+import random as rd
+
+# Liste beginnt mit eckigen Klammern
+emotionen = [":-)", ":-(", ":-o", ":-|"]  # Liste
+anzahl = rd.randrange(1, 11)
+for _ in range(anzahl):
+    emotion = rd.choice(emotionen)  # choice benötigt eine Liste
+    print(emotion)
