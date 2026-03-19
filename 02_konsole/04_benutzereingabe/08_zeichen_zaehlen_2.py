a4527aa11d7e5822446e2a4e997097438eaf5657
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-19 15:51:22 +0100

    Zeichen zählen 2

---
+++ 02_konsole/04_benutzereingabe/08_zeichen_zaehlen_2.py
@@ -0,0 +1,6 @@
+for _ in range(3):
+    name = input(f"Bitte geben Sie einen Namen ein:")
+    anzahl_zeichen = len(name)
+    print(f"Der Name {name} besitzt {anzahl_zeichen} Zeichen.")
+    print("================")
+    
